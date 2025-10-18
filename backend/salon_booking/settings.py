d62cac8412519715b23c834ff96f84a639a30a27
ramdar143 <marllouieburlat.bulayog@my.smciligan.edu.ph>
2025-10-19 00:32:54 +0800

    Use console email backend to fix SMTP timeout issues

--- backend/salon_booking/settings.py
+++ backend/salon_booking/settings.py
@@ -234,7 +234,9 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = 'accounts.User'
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Temporarily use console backend to avoid SMTP timeout issues
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
